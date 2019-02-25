18f152a767188f359bc2d44afe3340bc42f78669
svc-student <svc-student@student.directory.jccc>
2019-02-25 11:45:14 -0600

    puzzle stuff

---
+++ 250 - Resolve (Master)/Assets/PlayerItemTemp.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItemTemp : MonoBehaviour
+{
+    public float tempRadius = 5f;
+    public bool hot = false;
+    public bool cold = false;
+    public bool neither = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeItemTemp();
+        InTempZone();
+    }
+
+    public void InTempZone()
+    {
+        List<Collider> overlaps = new List<Collider>();
+        overlaps.AddRange(Physics.OverlapSphere(transform.position, tempRadius));
+
+        if (overlaps != null)
+        {
+            foreach(Collider collider in overlaps)
+            {
+                if (collider.gameObject.tag == "ChangeTempObject")
+                {
+                    if (hot)
+                    {
+                        collider.gameObject.GetComponent<HandleObjectTemperature>().hot = true;
+                        collider.gameObject.GetComponent<HandleObjectTemperature>().cold = false;
+                        collider.gameObject.GetComponent<HandleObjectTemperature>().neither = false;
+                        collider.gameObject.GetComponent<HandleObjectTemperature>().changeColor = true;
+                    }
+                    else if (cold)
+                    {
+                        collider.gameObject.GetComponent<HandleObjectTemperature>().hot = false;
+                        collider.gameObject.GetComponent<HandleObjectTemperature>().cold = true;
+                        collider.gameObject.GetComponent<HandleObjectTemperature>().neither = false;
+                        collider.gameObject.GetComponent<HandleObjectTemperature>().changeColor = true;
+                    }
+                }
+            }
+        }
+    }
+
+    public void ChangeItemTemp()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (hot)
+            {
+                hot = false;
+                cold = true;
+            }
+            else if (cold)
+            {
+                cold = false;
+                neither = true;
+            }
+            else if (neither)
+            {
+                neither = false;
+                hot = true;
+            }
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, tempRadius);
+    }
+}
---
+++ 250 - Resolve (Master)/Assets/PsychicControlledMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PsychicControlledMovement : MonoBehaviour
+{
+    public float movementSpeed = 4f;
+    public Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Controls();
+    }
+
+    private void Controls()
+    {
+        #region Face Cursor
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundTerrain = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundTerrain.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.red);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+        #endregion
+
+
+        #region Movement
+        Vector3 moveDirection = Vector3.zero;
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            //transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+            moveDirection += -mainCamera.transform.right;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            //transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+            moveDirection += mainCamera.transform.right;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            //transform.position += Vector3.forward * movementSpeed * Time.deltaTime;
+            Vector3 cameraForward = mainCamera.transform.forward;
+            cameraForward.y = 0;
+            moveDirection += cameraForward;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            //transform.position += Vector3.back * movementSpeed * Time.deltaTime;
+            Vector3 cameraBackward = -mainCamera.transform.forward;
+            cameraBackward.y = 0;
+            moveDirection += cameraBackward;
+        }
+
+        transform.position += moveDirection * Time.deltaTime * movementSpeed;
+        #endregion
+    }
+}
