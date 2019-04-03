56e7f489d2ce488b6b89695a20becdb024030efc
Nicolas R <43824118+onecalledNico@users.noreply.github.com>
2019-04-02 21:22:37 -0500

    Controller and Facing...
    Updated the character controller and moved the mouse facing mechanic to a new script.

--- 250 - Resolve (Master)/Assets/_Scripts/Player/PlayerController.cs
+++ 250 - Resolve (Master)/Assets/_Scripts/Player/PlayerController.cs
@@ -3,168 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
+[RequireComponent (typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    [HideInInspector]
-    public float movementSpeed;        //Speed for movement
-    public float staminaLossRate;
-    public float baseMovementSpeed;
-    public float climbingSpeed;
-    public bool movement = true;
-    public bool aiControl = false;
+    #region Movement and Slope Variables
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
 
-    private Rigidbody rbody;    //Rigid body for gravity
+    [SerializeField] private float walkSpeed, runSpeed;
+    [SerializeField] private float runBuildupSpeed;
+    [SerializeField] private KeyCode runKey;
+
+    private float moveSpeed;
+
+    [SerializeField] private float slopeforce;
+    [SerializeField] private float slopeForceRayLength;
+
+    private CharacterController charCont;
+    #endregion
 
     public Camera mainCamera;  //Camera for face cursor
-
     public bool climbing = false;
-    public bool transferClimb = false;
-    public bool allowFall = true;
-
-    public Transform wayPoint;
-
-    public bool canSprint = true;
     private bool canClimb;
     private Transform climbLookPosition;
+    public bool canSprint = true;
 
     public GameObject hackingGame;
+    public float staminaLossRate;
 
-    void Start()
+    public float baseMovementSpeed;
+    public bool movement = true;
+
+    private void Awake()
     {
-        rbody = GetComponent<Rigidbody>();
+        charCont = GetComponent<CharacterController>();
     }
+
 
     void Update()
     {
-        DetermineClimb();
-        MovementControls();
-    }
-
-
-    void FixedUpdate()
-    {
-            if (Input.GetKey(KeyCode.LeftShift) && canSprint)
-            {
-                movementSpeed = baseMovementSpeed * 2;
-                ConsumeStamina();
-            }
-
-        else
+        Movement();
+        if (Input.GetKey(KeyCode.LeftShift) && canSprint)
         {
-            movementSpeed = baseMovementSpeed;
+            ConsumeStamina();
         }
     }
 
-    private void MovementControls()
+    #region Movement and Slope
+    private void Movement()
     {
-        if (movement == true)
+        float horizInput = Input.GetAxis(horizontalInputName);
+        float vertInput = Input.GetAxis(verticalInputName);
+
+        Vector3 forwardMovement = mainCamera.transform.forward * vertInput;
+        Vector3 rightMovement = mainCamera.transform.right * horizInput;
+
+        charCont.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * moveSpeed);
+
+        if ((vertInput != 0 || horizInput != 0) && OnSlope())
         {
-            if (climbing == false)
-            {
-                GetComponent<Rigidbody>().useGravity = true;
-                #region Face Cursor
-                Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-                Plane groundTerrain = new Plane(Vector3.up, Vector3.zero);
-                float rayLength;
-
-                if (groundTerrain.Raycast(cameraRay, out rayLength))
-                {
-                    Vector3 pointToLook = cameraRay.GetPoint(rayLength);
-                    Debug.DrawLine(cameraRay.origin, pointToLook, Color.red);
-
-                    transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
-                }
-                #endregion
-
-
-                #region Movement
-                Vector3 moveDirection = Vector3.zero;
-                if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-                {
-                    //transform.position += Vector3.left * movementSpeed * Time.deltaTime;
-                    moveDirection += -mainCamera.transform.right;
-                }
-                if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-                {
-                    //transform.position += Vector3.right * movementSpeed * Time.deltaTime;
-                    moveDirection += mainCamera.transform.right;
-                }
-                if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-                {
-                    //transform.position += Vector3.forward * movementSpeed * Time.deltaTime;
-                    Vector3 cameraForward = mainCamera.transform.forward;
-                    cameraForward.y = 0;
-                    moveDirection += cameraForward;
-                }
-                if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-                {
-                    //transform.position += Vector3.back * movementSpeed * Time.deltaTime;
-                    Vector3 cameraBackward = -mainCamera.transform.forward;
-                    cameraBackward.y = 0;
-                    moveDirection += cameraBackward;
-                }
-
-                transform.position += moveDirection * Time.deltaTime * movementSpeed;
-                #endregion
-            }
-            else if (climbing == true)
-            {
-                #region Climbing
-                GetComponent<Rigidbody>().useGravity = false;
-                ConsumeStamina();
-
-                if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-                {
-                    transform.position += Vector3.left * climbingSpeed * Time.deltaTime;
-                }
-                if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-                {
-                    transform.position += Vector3.right * climbingSpeed * Time.deltaTime;
-                }
-                if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-                {
-                    transform.position += Vector3.up * climbingSpeed * Time.deltaTime;
-                }
-                if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-                {
-                    transform.position += Vector3.down * climbingSpeed * Time.deltaTime;
-                }
-
-                #endregion
-            }
+            charCont.Move(Vector3.down * charCont.height / 2 * slopeforce * Time.deltaTime);
         }
 
-        //if (aiControl == true)
-        //{
-        //    float step = climbingSpeed * Time.deltaTime;                                            // The step size is equal to climbing speed times frame time.
-        //    transform.position = Vector3.MoveTowards(transform.position, wayPoint.position, step);  //Move position closer to waypoint
-        //}
+        SetMovementSpeed();
     }
 
-    //#region AI Movement Control
-    //public void GotoWaypoint()
-    //{
-    //    rbody.useGravity = false;
-    //    movement = false;
-    //    aiControl = true;
-    //}
+    private void SetMovementSpeed()
+    {
+        if (Input.GetKey(runKey)) moveSpeed = Mathf.Lerp(moveSpeed, runSpeed, Time.deltaTime * runBuildupSpeed);
+        else moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, Time.deltaTime * runBuildupSpeed);
+    }
 
-    //public void StopWaypoint()
-    //{
-    //    aiControl = false;
-    //    climbing = true;
-    //    movement = true;
-    //}
+    private bool OnSlope()
+    {
+        RaycastHit hit;
 
-    //public void ResetMovement()
-    //{
-    //    aiControl = false;
-    //    rbody.useGravity = true;
-    //    climbing = false;
-    //    movement = true;
-    //}
-
-    //#endregion
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, charCont.height / 2 * slopeForceRayLength))
+        {
+            if (hit.normal != Vector3.up) return true;
+        }
+        return false;
+    }
+    #endregion
 
     private void DetermineClimb()
     {
@@ -226,5 +146,4 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
-
 }
